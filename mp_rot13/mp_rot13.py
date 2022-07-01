572e0c85c7f96a9c9f6d763259666c2053fa738f
EnzoFer <enzo.fernandez@hotmail.com>
2022-07-01 17:57:19 -0300

    mp_rot13

--- mp_rot13/mp_rot13.py
+++ mp_rot13/mp_rot13.py
@@ -15,12 +15,16 @@ def hijo2(process, queue):
     encripted_line = codecs.encode(line, "rot_13")
     queue.put(encripted_line)
 
-if __name__ == 'main':
+if __name__ == '__main__':
+    
     queue = mp.Queue()
     r, w = mp.Pipe()
+    
     process1 = mp.Process(target=hijo1, args=(r, queue))
     process2 = mp.Process(target=hijo2, args=(w, queue))
+    
     process1.start()
     process2.start()
+    
     process1.join()
     process2.join()
